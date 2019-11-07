9d43e0cbd46f97304b863677e3beb230d7844288
raohongsheng <raohongsheng>
2019-11-07 14:57:39 +0800

    Review_5_commit

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git abc ");
 		System.out.println("Git Review");
+		System.out.println("这里是版本2.0");
 	}
 }
